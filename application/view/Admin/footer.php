0bae1347f6977abfdf076a12975208e25497f326
吴威 <529778790@qq.com>
2021-08-19 16:39:34 +0800

    清空

--- application/view/Admin/footer.php
+++
@@ -1,8 +0,0 @@
-<?php if(!defined('APP_PATH')) {exit('error!');}?>
-</div>
-<div class="layui-footer">
-    欢迎使用 &copy; <a href="<?php echo PATH_URL;?>" target="_black"><?php echo configGet('title');?></a>
-</div>
-</div>
-</body>
-</html>
